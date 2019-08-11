2c145cce30579e3eb13c7060bfb73a9883c37ecd
ejdataras <ejdaras@gmail.com>
2019-08-11 23:24:16 +0200

    nearlyfinished

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Alcohol is more enjoyable if consumed in moderation", "Il n'y a pas de bonne fête sans lendemain", "Un lendemain de fête, tout fêtard devient phylosophe", "Un verre à moitié plein ou à moitié vide ça fait toujours une bonne soirée."],
+    typeSpeed: 100,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,8 @@
 import 'bootstrap';
 
 console.log('Hello World from Webpacker')
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
