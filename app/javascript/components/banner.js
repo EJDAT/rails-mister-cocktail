7e84c3b9f0d75f14f44330d228b83fe8346d6606
ejdataras <ejdaras@gmail.com>
2019-08-11 23:26:46 +0200

    phylolol

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Alcohol is more enjoyable if consumed in moderation", "Il n'y a pas de bonne fête sans lendemain", "Un lendemain de fête, tout fêtard devient phylosophe", "Un verre à moitié plein ou à moitié vide ça fait toujours une bonne soirée."],
+    strings: ["Alcohol is more enjoyable if consumed in moderation", "Il n'y a pas de bonne fête sans lendemain", "Un lendemain de fête, tout fêtard devient philosophe", "Un verre à moitié plein ou à moitié vide ça fait toujours une bonne soirée."],
     typeSpeed: 100,
     loop: true
   });
